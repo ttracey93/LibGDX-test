43fde85d620d6198e4fa9b169695fcabaf1b8edd
tbone <tylertracey09@gmail.com>
2015-01-21 11:28:53 -0500

    Initial Refactor of engine as per the architecture described.

--- core/src/com/mygdx/game/SaltFactory.java
+++ core/src/com/mygdx/game/SaltFactory.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.state.manager.GameStateManager;
 
-public class TestGame extends ApplicationAdapter {
+public class SaltFactory extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private GameStateManager gameStateManager;
 
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.TestGame;
+import com.mygdx.game.SaltFactory;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TestGame(), config);
+		new LwjglApplication(new SaltFactory(), config);
 	}
 }

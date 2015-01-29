f91d8e4ce68c45270a56b117cdcbeb7d19caa113
Riley <chaoko954@gmail.com>
2015-01-29 00:39:34 -0500

    new level

--- core/src/com/mygdx/game/ILevelName.java
+++ core/src/com/mygdx/game/ILevelName.java
@@ -7,7 +7,7 @@ public interface ILevelName {
     public static final String MAIN_MENU = "../Maps/mainMenu.tmx";
     public static final String TEST2 = "../Maps/map2.tmx";
     public static final String JUMPHEIGHT = "../Maps/jumpHeight.tmx";
-    public static final String HUBWORLD = "../Maps/hubworld.tmx";
+    public static final String HUBWORLD = "../Maps/newhubworld.tmx";
     public static final String WORLD1 = "../Maps/world1.tmx";
     public static final String FORESTHOUSE = "../Maps/foresthouse10.tmx";
     public static final String WORLD2 = "../Maps/world2.tmx";

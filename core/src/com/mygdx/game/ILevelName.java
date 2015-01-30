4397b051035334eeea8b96c06882c9cf15ff453f
Riley <chaoko954@gmail.com>
2015-01-29 21:08:29 -0500

    new level

--- core/src/com/mygdx/game/ILevelName.java
+++ core/src/com/mygdx/game/ILevelName.java
@@ -4,11 +4,5 @@ package com.mygdx.game;
  * Created by Dubforce on 1/21/2015.
  */
 public interface ILevelName {
-    public static final String MAIN_MENU = "../Maps/mainMenu.tmx";
-    public static final String TEST2 = "../Maps/map2.tmx";
-    public static final String JUMPHEIGHT = "../Maps/jumpHeight.tmx";
     public static final String HUBWORLD = "../Maps/newhubworld.tmx";
-    public static final String WORLD1 = "../Maps/world1.tmx";
-    public static final String FORESTHOUSE = "../Maps/foresthouse10.tmx";
-    public static final String WORLD2 = "../Maps/world2.tmx";
 }
